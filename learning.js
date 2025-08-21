9db49d2a99912e1aed69b415af3bea8a31624f17
Dennis Tei-Muno <munodennis@gmail.com>
2025-08-21 13:19:23 -0500

    Create learning.js

---
+++ learning.js
@@ -0,0 +1,11 @@
+// first time ever javascript basics
+console.log('JavaScript')
+console.log(2011)
+console.log('Woohoo! I love to code! #codecademy')
+console.log(20.49)
+console.log(26 + 3.5);
+console.log(2025 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
+console.log('Hello' + 'World')
+console.log('Hello' + ' ' + 'World')
