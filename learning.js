aaf1da9157932d7628b43f82e6b86888b051051a
Dennis Tei-Muno <munodennis@gmail.com>
2025-08-22 01:32:57 -0500

    Update learning.js

--- learning.js
+++ learning.js
@@ -9,3 +9,52 @@ console.log(65 / 240);
 console.log(0.2708 * 100);
 console.log('Hello' + 'World')
 console.log('Hello' + ' ' + 'World')
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+var favoriteFood = 'pizza'
+
+// let allows you to create a variable and then change the value later on
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+// constants
+const entree = 'Enchiladas';
+console.log(entree);
+
+//variables and mathematical expressions
+let levelUp = 10;
+levelUp +=5;
+let powerLevel = 9001;
+powerLevel -=100;
+let multiplyMe = 32;
+multiplyMe *=11;
+let quarterMe = 1152;
+quarterMe /=4;
+
+// decrease or increase value by 1
+let gainedDollar = 3;
+gainedDollar ++;
+let lostDollar = 50;
+lostDollar --;
+
+//interpolation
+var myName = 'Dennis';
+var myCity = 'Dallas';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+//type of data type
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
+
+// 
