c79e92cdb6aad739bbc5ebb87af26a5f751fbb78
Dennis Tei-Muno <munodennis@gmail.com>
2025-08-23 00:49:36 -0500

    Update learning.js

--- learning.js
+++ learning.js
@@ -64,3 +64,53 @@ if (hungerLevel > 7) {
 } else {
   console.log('We can eat later!');
 }
+
+/* ternary operators for true or false:
+instead of if or else you can use this */
+isNightTime ? console.log('Turn on the lights!') : console.log('Turn off the lights!');
+let isLocked = false;
+
+
+(isLocked) ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.')
+
+let isCorrect = true;
+
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!')
+
+let favoritePhrase = 'Love That!';
+
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!")
+// else if statements
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.');
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!');
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!')
+}
+else {
+  console.log('Invalid season.');
+}
+
+// switches:
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition){
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
